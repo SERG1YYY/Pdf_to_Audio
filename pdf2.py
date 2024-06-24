8649ae227de979265a45abc3726b59a84df037d1
SERGIYYY <135770267+SERG1YYY@users.noreply.github.com>
2024-06-25 00:38:54 +0300

    Add files via upload

--- pdf2.py
+++ pdf2.py
@@ -1,18 +1,19 @@
-import PyPDF2 # type: ignore
-import pyttsx3 # type: ignore
+import PyPDF2  # Ігноруємо потенційні попередження про типи від PyPDF2
+import pyttsx3 # Ігноруємо потенційні попередження про типи від pyttsx3
 
-with open(r'C:\Users\SERGIYYY\Desktop\pd.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    speaker = pyttsx3.init()
 
-    rate = speaker.getProperty('rate')
-    speaker.setProperty('rate', 100)
+with open(r'C:\Users\SERGIYYY\Desktop\Pdf_to_Audio\Ukraine.pdf', 'rb') as pdf_file: # Відкриваємо PDF-файл у двійковому режимі для читання
+    reader = PyPDF2.PdfReader(pdf_file) # Створюємо об'єкт PdfReader для читання файлу
+    speaker = pyttsx3.init() # Ініціалізуємо об'єкт для перетворення тексту в мовлення
 
-    for pagenumber in range(len(reader.pages)):
-        page = reader.pages[pagenumber]
-        text = page.extract_text()
-        speaker.say(text)
-        speaker.runAndWait()
+    rate = speaker.getProperty('rate') # Отримуємо поточну швидкість мовлення
+    speaker.setProperty('rate', 150) # Встановлюємо нову швидкість мовлення (150 слів за хвилину)
 
-    speaker.save_to_file(text, r'C:\Users\SERGIYYY\Desktop\audio.mp3')
-    speaker.runAndWait()
+    for pagenumber in range(len(reader.pages)): # Цикл по всіх сторінках PDF-файлу
+        page = reader.pages[pagenumber] # Отримуємо поточну сторінку
+        text = page.extract_text() # Витягуємо текст зі сторінки
+        speaker.say(text) # Перетворюємо текст у мовлення і відтворюємо його
+        speaker.runAndWait() # Перетворюємо текст у мовлення і відтворюємо його
+
+    speaker.save_to_file(text, r'C:\Users\SERGIYYY\Desktop\Pdf_to_Audio\result.mp3') # Зберігаємо озвучений текст у MP3-файл. # УВАГА: цей рядок зберігає лише текст останньої сторінки!
+    speaker.runAndWait() # Чекаємо завершення запису у файл
